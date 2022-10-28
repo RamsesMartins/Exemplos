af5b56213f0984bf4d9f8c728ad7f4aed1135c96
Ramses de Oliveira Martins <ramses4323@gmail.com>
2022-10-28 14:18:34 +0000

    Terminado

--- SESI/Artigo-Roger/js/main.js
+++ SESI/Artigo-Roger/js/main.js
@@ -1,17 +1,60 @@
-/********************************
- * DECLARANDO VARIÁVEIS GLOBAIS *
- ********************************/
-
 function Main(){
-    var alfabeto = ['a','b', 'c', 'd', 'e', 'f', 'g', 'h','i','j', 'k','l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    var alfabeto = ['a','b', 'c', 'd', 'e', 'f', 'g', 'h','i','j', 'k','l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
+    // GETS
     var txtCripto = document.getElementById("txtCripto")
     var txtDescrip = document.getElementById("txtDescrip")
-    var chave = document.getElementById("chave")
-    if(chave.value == ""){
+    var chave = parseInt(document.getElementById("chave").value)
+    var txtCripto
+    var resultado = new Array()
+
+    if(chave == ""){
         alert("Preencha o campo chave!")
     }
-    if(txtCripto.value == "" && txtDescrip == ""){
+    else if(txtCripto.value == "" && txtDescrip == ""){
         alert("Preencha algum dos campos")
+    }else if(txtCripto.value == ""){
+        txt = txtDescrip.value
+        var arr = []
+        arr = txt.split('')
+        for(var i=0; i < arr.length; i++){
+            
+            for(var abc=0; abc < 30; abc++){
+                var index
+                if(alfabeto[abc] == arr[i]){
+                    if(alfabeto[abc] == ' '){
+                        index= abc
+                    }else{
+                        index = abc+chave
+                    }                    
+                    resultado.push(alfabeto[parseInt(index)])
+                    console.log(resultado) 
+                }
+            }
+                       
+        }
+        txtCripto.innerHTML = resultado.join('')
+        
+    }else if(txtDescrip.value == ""){
+        txt = txtCripto.value
+        var arr = []
+        arr = txt.split('')
+        for(var i=0; i < arr.length; i++){
+            
+            for(var abc=0; abc < 30; abc++){
+                var index
+                if(alfabeto[abc] == arr[i]){
+                    if(alfabeto[abc] == ' '){
+                        index= abc
+                    }else{
+                        index = abc-chave
+                    }                    
+                    resultado.push(alfabeto[parseInt(index)])
+                    console.log(resultado) 
+                }
+            }
+                       
+        }
+        txtDescrip.innerHTML = resultado.join('')
     }
     
 }
